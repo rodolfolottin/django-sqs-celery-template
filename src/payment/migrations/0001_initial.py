2c1b35752a225b3676187fe9eae78e83df13c3ca
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2020-07-01 21:30:23 -0300

    style: Black it

--- src/payment/migrations/0001_initial.py
+++ src/payment/migrations/0001_initial.py
@@ -7,15 +7,22 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Payment',
+            name="Payment",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('value', models.IntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("value", models.IntegerField()),
             ],
         ),
     ]
